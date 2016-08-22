a418e626a1f8cd34f87695a0c65e49ba299cc758
shreya22 <shreya.iiitm@gmail.com>
2016-08-22 11:53:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var express= require('express');
+var app= express();
+var mongojs= require('mongojs');
+var db= mongojs('contactlist', ['contactlist']);
+var bodyParser= require('body-parser');
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function(req, res){
+	console.log('recieved a get request!');
+
+	db.contactlist.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+
+});
+
+app.post('/contactlist', function(req, res){
+	console.log(req.body);
+
+	db.contactlist.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id', function(req, res){
+	var id=req.params.id;
+	console.log(id);
+
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.get('/contactlist/:id', function(req, res){
+	var id= req.params.id;
+	console.log(id);
+
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id', function(req, res){
+	var id= req.params.id;
+	console.log(req.body.name);
+
+	db.contactlist.update({_id:mongojs.ObjectId(id)}, {name:req.body.name, email:req.body.email, number:req.body.number}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.listen(3001);
+console.log('server running on port 3001');
